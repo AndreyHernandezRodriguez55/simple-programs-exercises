847ca8da9e10529c1d9d1b07839332adac492251
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-29 12:54:26 -0500

    solucion ejercicio circulo

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
-#Ingrese su nombre: Perico
-#Hola, Perico
+#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
+#Ingrese el radio: 5
+#Perimetro: 31.4
+#Área: 78.5
+import math
 
-name= input("enter your name: ")
-print("Hello, ", name)
+radio = int(input("Escribe el radio de tu círculo: "))
+
+area = math.pi * (radio ** 2)
+perimetro = 2 * math.pi * radio
+
+print(f"""De acuerdo al radio, tu área es: {area} y tu perímetro es: {perimetro}""")
