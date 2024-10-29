680cb2bc2efb135bf8c2fb7b57f1f23a5560c41e
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-29 13:05:53 -0500

    solucion ejercicio circulo en ingles

--- main.py
+++ main.py
@@ -3,10 +3,7 @@
 #Perimetro: 31.4
 #Área: 78.5
 import math
-
-radio = int(input("Escribe el radio de tu círculo: "))
-
+radio = int(input("Write the radius of your circle: "))
 area = math.pi * (radio ** 2)
-perimetro = 2 * math.pi * radio
-
-print(f"""De acuerdo al radio, tu área es: {area} y tu perímetro es: {perimetro}""")
+perimeter = 2 * math.pi * radio
+print(f"""According to the radius, your area is: {area} and your perimeter is: {perimeter }""")
