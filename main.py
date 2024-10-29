6a9f2de2a4d3f8bae3546d008cc4816f2c5ead82
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-29 13:13:42 -0500

    solucion ejercicio promedio

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
-#Ingrese el radio: 5
-#Perimetro: 31.4
-#Área: 78.5
-import math
-radio = int(input("Write the radius of your circle: "))
-area = math.pi * (radio ** 2)
-perimeter = 2 * math.pi * radio
-print(f"""According to the radius, your area is: {area} and your perimeter is: {perimeter }""")
+#Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
+#Primera nota: 55
+#Segunda nota: 71
+#Tercera nota: 46
+#Cuarta nota: 87
+#El promedio es: 64.75
+
+note1 = float(input("enter your first note: "))
+note2 = float(input("Enter your second note : "))
+note3 = float(input("Enter your third note : "))
+note4 = float(input("enter your fourth note: "))
+
+average = (note1 + note2 + note3 + note4) / 4
+print(f"the average is: {average:.2f}")
