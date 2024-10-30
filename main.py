a4045a8e232426f1969384fb687bb4c88023c23a
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-30 10:21:24 -0500

    solucion ejericico Parte decimal

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-#Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
+#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
 
-#Hora actual: 3
-#Cantidad de horas: 5
-#En 5 horas, el reloj marcara las 8
-#Hora actual: 11
-#Cantidad de horas: 43
-#En 43 horas, el reloj marcara las 6
+#Ingrese un numero: 4.5
+#0.5
+#Ingrese un numero: -1.19
+#0.19
 
 
-current_time= int(input("current time: "))
-hour_quantity= int(input("Chour quantity:  "))
+nomber= float(input("Enter a number: "))
 
-new_time=(hour_quantity + current_time) %12
+decimal_part = abs(nomber) - abs(int(nomber))
 
-if new_time == 0:
-    new_time= 12
-
-    print(f"In {current_time} hours, the clock will read {new_time} ")
+print(decimal_part)
